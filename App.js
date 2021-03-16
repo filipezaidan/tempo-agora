d4c9d0254ac115a60bb930b854bee32d8c04abd1
Filipe Zaidan <41112779+filipezaidan@users.noreply.github.com>
2021-03-15 23:10:05 -0300

    Initial commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import Routes from './src/routes';
+
+export default function App(){
+  return(
+    <NavigationContainer>
+      <Routes/>
+    </NavigationContainer>
+  );
+}
