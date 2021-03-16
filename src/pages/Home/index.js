1a456f8d8b01b8b4612015a0a0d7ca335130a916
Filipe Zaidan <41112779+filipezaidan@users.noreply.github.com>
2021-03-15 23:50:21 -0300

    Create button Menu

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { SafeAreaView, Text, StyleSheet} from 'react-native';
 
+import Menu from '../../components/Menu';
 
 export default function Home(){
     return(
-        <View>
-            <Text>Página Home</Text>
-        </View>
+        <SafeAreaView style={styles.container}>
+            <Menu/>
+        </SafeAreaView>
     );
 }
+
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#e8f0ff',
+        paddingTop: '5%',
+
+    }
+});
